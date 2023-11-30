26d403fc45494f0081d07a4f77948e9b051cb994
Apollostowel <apollostowel@icloud.com>
2023-12-01 01:05:20 +0200

    Building search

--- base/views.py
+++ base/views.py
@@ -1,5 +1,5 @@
 from typing import Any
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
@@ -33,6 +33,12 @@ class RegisterPage(FormView):
             login(self.request, user)
 
         return super(RegisterPage, self).form_valid(form)
+    
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        
+        return super(RegisterPage, self).get(*args, **kwargs)
 
 class TaskList(LoginRequiredMixin, ListView):
     model =  Task
@@ -41,7 +47,15 @@ class TaskList(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
-        context['count'] = context['tasks'].filter(complete=False)
+        context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+
+        if search_input:
+            context['tasks'] = context['tasks'].filter(
+                title__icontains=search_input)
+
+        context['search_input'] = search_input
 
         return context
 
